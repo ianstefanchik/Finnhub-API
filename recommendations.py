21a341c6a2f4bd6bde4d52d9fef519777de957b8
Ian <ianstefanchik@gmail.com>
2025-01-26 00:33:46 -0500

    Analyst Recommendations

---
+++ recommendations.py
@@ -0,0 +1,8 @@
+import finnhub
+
+api_key = 'YOUR_API_KEY'
+finnhub_client = finnhub.Client(api_key=api_key)
+
+symbol = 'AAPL'
+recommendations = finnhub_client.recommendation_trends(symbol)
+print(recommendations)
